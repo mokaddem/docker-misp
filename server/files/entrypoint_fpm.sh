ad2a8f81bea2929136f9066672a809cc82f074e9
Raphaël Vinot <raphael@vinot.info>
2023-01-19 14:05:33 +0100

    fix: Avoid high-use instances to fail.

--- server/files/entrypoint_fpm.sh
+++ server/files/entrypoint_fpm.sh
@@ -2,12 +2,13 @@
 
 change_php_vars(){
     for FILE in /etc/php/*/fpm/php.ini
-    do  
+    do
         [[ -e $FILE ]] || break
         sed -i "s/memory_limit = .*/memory_limit = 2048M/" "$FILE"
         sed -i "s/max_execution_time = .*/max_execution_time = 300/" "$FILE"
         sed -i "s/upload_max_filesize = .*/upload_max_filesize = 50M/" "$FILE"
         sed -i "s/post_max_size = .*/post_max_size = 50M/" "$FILE"
+        sed -i "s/pm.max_children = .*/pm.max_children = 100/" "$FILE"
     done
 }
 
