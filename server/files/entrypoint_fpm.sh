85d753078c4ea74cb9ecec2bcaf5669deb59965a
Raphaël Vinot <raphael@vinot.info>
2023-01-25 13:31:21 +0100

    fix: change var in the right file.

--- server/files/entrypoint_fpm.sh
+++ server/files/entrypoint_fpm.sh
@@ -10,6 +10,11 @@ change_php_vars(){
         sed -i "s/post_max_size = .*/post_max_size = 50M/" "$FILE"
         sed -i "s/pm.max_children = .*/pm.max_children = 100/" "$FILE"
     done
+    for FILE in /etc/php/*/fpm/pool.d/www.conf
+    do
+        [[ -e $FILE ]] || break
+        sed -i "s/pm.max_children = .*/pm.max_children = 100/" "$FILE"
+    done
 }
 
 echo "Configure PHP  | Change PHP values ..." && change_php_vars
